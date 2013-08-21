b046c56239e5c23ada6261427d5dd754ba6eafce
Max Livingston <maxliving@gmail.com>
2013-08-20 21:40:14 -0400

    Adding the main R program
    Nothing written yet, just setting up

---
+++ stop_frisk.R
@@ -0,0 +1,6 @@
+# stop_frisk.R
+# Created 8/20/2013 by Max Livingston
+# Last Modified: 8/20/2013
+# Some basic stats/plotting of NYC Stop and Frisk data
+# Data obtained from the ACLU: http://www.nyclu.org/content/stop-and-frisk-data
+
