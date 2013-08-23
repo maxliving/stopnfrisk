25d8175a5b20e4a5db89d490e6b5ff22d55f4edd
Max Livingston <maxliving@gmail.com>
2013-08-22 23:27:58 -0400

    testing

--- stop_frisk.R
+++ stop_frisk.R
@@ -4,4 +4,18 @@
 # Some basic stats/plotting of NYC Stop and Frisk data
 # Data obtained from the ACLU: http://www.nyclu.org/content/stop-and-frisk-data
 
-# test edits
+library(rgeos)
+library(maps)
+#library(gpclib)
+library(maptools)
+
+library(rgdal)
+library(ggplot2)
+library(plyr)
+library(mapproj)
+
+# Basic setup:
+setwd("~/Projects/stopnfrisk")
+data <- read.csv("data/SQF_2012.csv")
+dim(data)
+str(data)
