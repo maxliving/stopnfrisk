a8561a758ae83c40ef801b3b5f0e52fe90fd4421
Max Livingston <maxliving@gmail.com>
2013-09-08 17:48:57 -0400

    Able to plot stops on top of the NYC precinct map.
    Need to check on whether the projections are an issue.

--- stop_frisk.R
+++ stop_frisk.R
@@ -16,15 +16,19 @@ library(mapproj)
 ### Basic setup
 setwd("~/Projects/stopnfrisk")
 dfrm <- read.csv("data/SQF_2012.csv")
-precinctmap <- readOGR("data/nyc_precinctmap","nypp")
 
-## Set up a map dataframe that ggplot can use (see https://github.com/hadley/ggplot2/wiki/plotting-polygon-shapefiles)
+### Set up a map dataframe that ggplot can use (see https://github.com/hadley/ggplot2/wiki/plotting-polygon-shapefiles)
 precinctmap <- readOGR("data/nyc_precinctmap","nypp")
 precinctmap@data$id <- rownames(precinctmap@data)
-
 ## Normally I use fortify(precinctmap,region="id") but I got a weird error message when I did that, and the whole process worked when I took it out. Should look into that at some point.
 precinctmap.points <- fortify(precinctmap)
 precinctmap.df <- join(precinctmap.points,precinctmap@data,by="id")
+
+## Now that we've readied the precinctmap polygons, we need to prepare the points (from the csv)
+stops <- fortify(dfrm)
+stops$long <- stops$xcoord
+stops$lat <- stops$ycoord
+
 
 dfrm$race <- factor(dfrm$race, 
     levels=c(1:6), 
@@ -57,10 +61,27 @@ map <- ggplot(dfrm) + geom_point(aes(xcoord,ycoord,color=arrest.f),size=0.3) +
 ggsave("output/arrests_v2.png")
 
 
-## Too many points! Hard to tell 
+## Too many points! Hard to tell what's going on, so experimenting with contours:
 map <- ggplot(dfrm,aes(xcoord,ycoord))
 map + geom_density2d()
 ggsave("output/contourmap.png")
 map <- map + geom_point(size=0.3) + geom_density2d()
 ggsave("output/contourmap_wpoints.png")
 
+map <- ggplot(dfrm,aes(xcoord,ycoord))
+map + geom_density2d()
+ggsave("output/contourmap.png")
+map <- map + geom_point(size=0.3) + geom_density2d()
+ggsave("output/contourmap_wpoints.png")
+
+map <- ggplot(dfrm,aes(xcoord,ycoord))
+map <- map + stat_density2d(aes(fill = ..level..), geom="polygon")
+map + scale_fill_gradient2(space="rgb",low="white",high="red") + geom_point(size=0.2)
+
+pctmap <- ggplot(precinctmap.df,aes(long,lat,group=group)) + geom_polygon() + geom_path(color="white") + scale_fill_brewer()
+
++ geom_polygon(fill="white",color="grey50")
+
+# Bring in the polygon shapefile of NYC police precincts and plot the stops on that map:
+pctmapplot <- ggplot(precinctmap.df,aes(x=long,y=lat,group=group)) + geom_path(size=.3)
+pctmapplot + geom_point(data=stops,aes(group=NULL),size=.1,color="#D55E00")
